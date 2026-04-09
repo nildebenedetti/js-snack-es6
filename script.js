a4ac7354e870f43ae123ecd9a8a1b85a86c3a4e6
“Nilde <nilde.benedetti@gmail.com>
2026-04-09 14:48:39 +0200

    add: arrray con nomi delle bici

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('hello');
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+const scuderiaBici = [ /* gwendalin, catapult4, mazzoRazzo, ruotaLiscia*/];
+
