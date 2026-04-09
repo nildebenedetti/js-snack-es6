d1766c6ed73ad33a9e1687902f762d9a53ca5725
“Nilde <nilde.benedetti@gmail.com>
2026-04-09 15:22:55 +0200

    scorro l'array con il mio for of, per confrontare  i pesi. Ma il pc e'discalculico

--- script.js
+++ script.js
@@ -7,25 +7,25 @@ const scuderiaBici = [];
 
 const gwendalin = {
     nome: 'gwendalin',
-    peso: '11 kg',
+    peso: '11',
     colore: 'azzurro cielo'
 };
 
 const catapult4 = {
     nome: 'catapult4',
-    peso: '9 kg',
+    peso: '9',
     colore: 'verde lime'
 };
 
 const mazzoRazzo = {
     nome: 'mazzoRazzo',
-    peso: '7 kg',
+    peso: '7',
     colore: 'rosso fiamma'
 };
 
 const ruotaLiscia = {
     nome: 'ruotaLiscia',
-    peso: '10 kg',
+    peso: '10',
     colore: 'grafite'
 };
 
@@ -34,3 +34,15 @@ scuderiaBici.push(gwendalin,catapult4,mazzoRazzo,ruotaLiscia);
 // guardo se ci sono
 console.log(scuderiaBici);
 
+// stampare bici con peso minore
+// metodo for...of
+
+let minObject = scuderiaBici[0];
+
+for (const bici of scuderiaBici) {
+    if (bici.peso < minObject.peso) {
+        minObject = bici;
+    }
+}
+
+console.log(minObject);
